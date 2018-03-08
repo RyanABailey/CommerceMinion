68bb96b6653ba43f595adb54cc7f85bf2fd3de5d
Ryan <ryan.bailey@akqa.com>
2018-03-09 11:37:29 +1300

    Initial commit

---
+++ ConfigureSitecore.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigureSitecore.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2017
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Plugin.MyProject.Import
+{
+    using System.Reflection;
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore.Commerce.Core;
+    using Sitecore.Framework.Configuration;
+    using Sitecore.Framework.Pipelines.Definitions.Extensions;
+
+    /// <summary>
+    /// The configure sitecore class.
+    /// </summary>
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+        /// <summary>
+        /// The configure services.
+        /// </summary>
+        /// <param name="services">
+        /// The services.
+        /// </param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.RegisterAllPipelineBlocks(assembly);
+
+            services.Sitecore()
+                            .Pipelines(
+                                pipeLineConfig =>
+                                pipeLineConfig.ConfigurePipeline<IImportMinionMinionPipeline>(
+                                    config => config.Add<ImportMinionBlock>()
+                                                    ));
+
+            services.RegisterAllCommands(assembly);
+        }
+    }
+}
